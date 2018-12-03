4d58cbf0a49fc5992ece1f24192654c60e4eed97
JP <johnmcgrady@hotmail.co.uk>
2018-12-03 14:40:44 +0900

    Add Utils class with appropriate methods for retrieving data from API.

--- app/src/main/java/com/example/android/weather/MainActivity.java
+++ app/src/main/java/com/example/android/weather/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.weather;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     // URL for the weather data from the OpenWeatherMap website.
     private static final String WEATHER_REQUEST_URL =
-            "INSERT API HERE";
+            "http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=51925842ffff00a9ea6b84970bd7321e";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +17,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Create an AsyncTask to perform the HTTP request on the given URL.
         // When the result is received, update the UI.
-        /*WeatherAsyncTask task = new WeatherAsyncTask();
-        task.execute(WEATHER_REQUEST_URL);*/
+        WeatherAsyncTask task = new WeatherAsyncTask();
+        task.execute(WEATHER_REQUEST_URL);
     }
 
+    private void updateUi(Event results) {
+
+    }
+
+
     // To perform a network request on a background threat, and update the UI.
-   /* private class WeatherAsyncTask<String, Void, Event> {
+    private class WeatherAsyncTask extends AsyncTask<String, Void, Event> {
 
         // Runs on a background thread to make the network request.
         // Don't update the UI in the background thread, do it after it is finished.
@@ -41,5 +47,5 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         updateUi(result);
-    }*/
+    }
 }
--- app/src/main/java/com/example/android/weather/Utils.java
+++ app/src/main/java/com/example/android/weather/Utils.java
@@ -7,12 +7,109 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Utils {
 
     private static final String TAG = "Utils";
 
+    // Retrieves the weather data from the API and returns an Event
+    public static Event fetchWeatherData(String urlRequest) {
+        // Create URL object
+        URL url = createUrlObject(urlRequest);
+
+        // Perform a HTTP request to the URL and return a JSON response
+        String jsonResponse = "";
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(TAG, "Problem making the HTTP request", e);
+        }
+
+        Event weather = extractDataFromJson(jsonResponse);
+
+        return weather;
+    }
+
+    // Create a URL object from a URL String
+    private static URL createUrlObject(String urlRequest) {
+        URL url = null;
+
+        try {
+            url = new URL(urlRequest);
+        } catch (MalformedURLException exception) {
+            Log.e(TAG, "Error creating URL", exception);
+            return null;
+        }
+
+        return url;
+    }
+
+    // Make a HTTP request to the given URL and return a String.
+    // The String is the entire JSON response from the server which needs to be parsed.
+    private static String makeHttpRequest(URL url) throws IOException {
+        // Create an empty String to put the JSON response into
+        String jsonResponse = "";
+
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        // Create a HttpURLConnection and InputStream object ready for assignment
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.connect();
+
+            // If the request was successful (response code 200) read the input stream
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(TAG, "Error response code " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Problem resolving the weather JSON results", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    // Covert the InputStream into a String which contains the whole JSON response
+    // for the server
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
     // Access the appropriate data in the API and returns and Event object with the data.
-    public Event extractDataFromJson(String weatherJSON) {
+    public static Event extractDataFromJson(String weatherJSON) {
         // If the JSON URL is empty, return null.
         if (TextUtils.isEmpty(weatherJSON)) {
             Log.v(TAG, "JSON string is empty or null");
